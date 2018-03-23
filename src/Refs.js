839a3302bbf3ebc17765129724f2faa7cd3c45dc
uptonking <jinyaoo86@gmail.com>
2018-03-23 17:27:30 +0800

    add: init

---
+++ src/Refs.js
@@ -0,0 +1,45 @@
+import {typeNumber} from './utils'
+
+function setRef(Vnode, instance, domNode) {
+    if (instance) {
+        const refType = typeNumber(Vnode.ref)
+        if (refStrategy[refType]) {
+            refStrategy[refType](Vnode, Vnode.owner, domNode)
+        }
+    }
+}
+
+function clearRefs(refs) {
+    if (typeof refs === 'function') {
+        refs(null)
+    } else {
+        for (let refName in refs) {
+            refs[refName] = null
+        }
+    }
+}
+
+const refStrategy = {
+    3: function (Vnode, instance, domNode) {
+        if (Vnode._instance) {
+            instance.refs[Vnode.ref] = Vnode._instance
+        } else {
+            instance.refs[Vnode.ref] = domNode
+        }
+    },
+    4: function (Vnode, instance, domNode) {
+        refStrategy[3](Vnode, instance, domNode)
+    },
+    5: function (Vnode, instance, domNode) {
+        if (Vnode._instance) {
+            Vnode.ref(Vnode._instance)
+        } else {
+            Vnode.ref(domNode)
+        }
+    }
+}
+
+export {
+    setRef,
+    clearRefs,
+}
---
+++ src/index.js
@@ -0,0 +1,21 @@
+//@flow
+import { createElement as createEle } from './createElement'
+import { cloneElement } from './cloneElement'
+import { Children as child } from './Children'
+import { render, findDOMNode, createPortal } from './vdom'
+import { ReactClass } from './component'
+
+const React = {
+    findDOMNode,
+    // babel的默认设置是调用createElement这个函数
+    createElement: createEle,
+    render,
+    cloneElement,
+    createPortal,
+    Children: child,
+    Component: ReactClass
+}
+export const Component = ReactClass
+export const Children = child
+export const createElement = createEle
+export default React
